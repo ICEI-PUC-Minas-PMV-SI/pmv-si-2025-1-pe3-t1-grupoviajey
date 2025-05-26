280de82e22fa1dbc3450cb86f22b1fa3036ede1a
Amanda Mags <amdmagsdev@gmail.com>
2025-05-26 15:05:05 -0300

    chore: remove old src folder with outdated code

--- src/dev-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script para iniciar o servidor em modo de desenvolvimento usando nodemon
-
-echo "Iniciando servidor Viajey em modo de desenvolvimento..."
-npx nodemon server.js
--- src/init-db/migrations/20250508205500_password_to_password_hash.sql
+++
@@ -1,2 +0,0 @@
--- Renomeia a coluna 'password' para 'password_hash' na tabela 'users'
-ALTER TABLE users RENAME COLUMN password TO password_hash;
