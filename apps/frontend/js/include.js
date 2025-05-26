a9484f880e834dcf03074ca0436ef22e725d2df7
Amanda Mags <amdmagsdev@gmail.com>
2025-05-26 02:16:55 -0300

    Add Place Details Modal with dropdown functionality for adding to itinerary

--- apps/frontend/js/include.js
+++ apps/frontend/js/include.js
@@ -1,13 +1,19 @@
 // include.js
-function includeHTML(id, file) {
+function includeHTML(id, file, callback) {
   fetch(file)
     .then(response => response.text())
     .then(data => {
       document.getElementById(id).innerHTML = data;
+      if (typeof callback === 'function') callback();
     });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   includeHTML('header', 'components/header/header.html');
   includeHTML('footer', 'components/footer/footer.html');
+  includeHTML('place-details-modal-include', 'components/modal/PlaceDetailsModal.html', function() {
+    var script = document.createElement('script');
+    script.src = 'components/modal/PlaceDetailsModal.js';
+    document.body.appendChild(script);
+  });
 }); 
