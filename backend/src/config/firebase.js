ad7dc4b2dd225ac283707f349a85c9ffb1a102d7
Amanda Magalhaes <amdmagsdev@gmail.com>
2025-06-22 20:13:23 -0300

    feat(dependencies): add multer for file handling and update package-lock
    - Introduced multer as a dependency for handling file uploads.
    - Updated package.json and package-lock.json to reflect the addition of multer and its dependencies.
    - Enhanced server.js with improved console log formatting.
    - Added storage bucket configuration in firebase.js for Firebase integration.

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -5,7 +5,8 @@ const serviceAccount = require(path.resolve(__dirname, 'credencialFirebase.json'
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: 'viajey-db.firebasestorage.app'
   });
 }
 
